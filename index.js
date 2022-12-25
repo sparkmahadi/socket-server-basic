af3aebacc1a4367f33306e4c303a9e7fd2dba427
sparkmahadi <mahadi.development@gmail.com>
2022-12-25 13:15:05 +0600

    broadcasting and filter with name specific

--- index.js
+++ index.js
@@ -8,23 +8,33 @@ const httpServer = http.createServer(app);
 const {Server} = require('socket.io');
 const io = new Server(httpServer);
 
-io.on('connection', (socket)=>{
-    console.log('new user connected');
+// io.on('connection', (socket)=>{
+//     console.log('new user connected');
 
-    socket.on("disconnect", ()=>{
-        console.log('user disconnected');
-    })
+//     socket.on("disconnect", ()=>{
+//         console.log('user disconnected');
+//     })
 
-    socket.send("fifa world cup 2022");
+//     socket.send("fifa world cup 2022");
     
-    socket.on("testEvent", (data)=>{
-        console.log(data);
-    })
+//     // socket.on("testEvent", (data)=>{
+//     //     console.log(data);
+//     // })
 
+//     // broadcasting
+//     io.sockets.emit("fifaWorldCup", "hello football");
+// });
 
+// filtered broadcasting
+let fifa = io.of("/worldCup");
+fifa.on("connection", (socket)=>{
+    fifa.emit("worldCupEvent", "Hello! This is a filtered broadcast from fifa")
+})
 
-
-});
+let icc = io.of("/cricketCup");
+icc.on("connection", (socket)=>{
+    icc.emit("cricketCupEvent", "Hello! This is a filtered broadcast from icc world cup")
+})
 
 
 app.get('/', (req, res)=>{
